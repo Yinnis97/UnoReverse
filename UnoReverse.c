5df3e23076f1c5c97712a1ebb509c8bbe7b7398a
yinnis <12302157@student.pxl.be>
2024-05-16 10:30:09 +0200

    Fixed client_address_string bug

--- UnoReverse.c
+++ UnoReverse.c
@@ -36,38 +36,44 @@
 	void OSCleanup( void ) {}
 #endif
 
-
-char client_address_string[INET6_ADDRSTRLEN];  //String voor IP address van Client.
+//----------------------------------------------------------------------------------
+struct client_struct
+{
+	char client_address_string[INET6_ADDRSTRLEN]; //String voor IP address van Client.
+};
 int initialization();
-int connection( int internet_socket , FILE *filePointer);
-void execution( int internet_socket ,char client_address_string );
-void cleanup( int internet_socket, int client_internet_socket , int HTTP_socket );
+int connection( int internet_socket , FILE *filePointer , char * client_address_string,int size);
+void execution( int internet_socket ,  const char *client_address_string , FILE *filePointer);
+void cleanup(int client_internet_socket);
 int initialization_HTTP();
-void execution_HTTP(int internet_socket , FILE *filePointer , char client_address_string);
+void execution_HTTP( FILE *filePointer ,const char *client_address_string);
+
+//----------------------------------------------------------------------------------
+
 int main( int argc, char * argv[] )
 {
 	FILE *filePointer =fopen( "data.log", "w" );
-	
 	OSInit();
-    int *client_internet_socket;
 	int internet_socket = initialization();
-	int HTTP_socket = initialization_HTTP();
-	
+    char client_address_string[INET6_ADDRSTRLEN];
 while(1)
 {
-	int client_internet_socket = connection( internet_socket ,filePointer );
-	execution( client_internet_socket , client_address_string);
-	execution_HTTP(HTTP_socket, filePointer , client_address_string);
+	int client_internet_socket = connection( internet_socket ,filePointer ,client_address_string,sizeof(client_address_string));
+	struct client_struct* client_s = (struct client_struct*)malloc(sizeof(struct client_struct));
+	strcpy(client_s->client_address_string, client_address_string);
+	execution( client_internet_socket , client_address_string , filePointer);
+	cleanup( client_internet_socket);
 }
 
-    fclose(filePointer);
-	cleanup( internet_socket, client_internet_socket , HTTP_socket);
+    close( internet_socket);
+    fclose(filePointer);	
 	OSCleanup();
 
 	return 0;
 }
 
 //----------------------------------------------------------------------------------
+
 int initialization()
 {
 	//Step 1.1
@@ -77,7 +83,7 @@ int initialization()
 	internet_address_setup.ai_family = AF_UNSPEC;
 	internet_address_setup.ai_socktype = SOCK_STREAM;
 	internet_address_setup.ai_flags = AI_PASSIVE;
-	int getaddrinfo_return = getaddrinfo( NULL, "22", &internet_address_setup, &internet_address_result );
+	int getaddrinfo_return = getaddrinfo( "fe80::3e10:67d5:f983:9996", "22", &internet_address_setup, &internet_address_result );
 	if( getaddrinfo_return != 0 )
 	{
 		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
@@ -136,11 +142,13 @@ int initialization()
 
 int initialization_HTTP()
 {
-struct addrinfo internet_address_setup;
+	struct addrinfo internet_address_setup;
 	struct addrinfo * internet_address_result;
 	memset( &internet_address_setup, 0, sizeof internet_address_setup );
-	internet_address_setup.ai_family = AF_INET;
+	internet_address_setup.ai_family = AF_UNSPEC;
 	internet_address_setup.ai_socktype = SOCK_STREAM;
+	internet_address_setup.ai_flags = AI_PASSIVE;
+
 	int getaddrinfo_return = getaddrinfo( "ip-api.com", "80", &internet_address_setup, &internet_address_result );
 	if( getaddrinfo_return != 0 )
 	{
@@ -152,7 +160,7 @@ struct addrinfo internet_address_setup;
 	struct addrinfo * internet_address_result_iterator = internet_address_result;
 	while( internet_address_result_iterator != NULL )
 	{
-		//Step 1.2
+		
 		internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
 		if( internet_socket == -1 )
 		{
@@ -160,7 +168,7 @@ struct addrinfo internet_address_setup;
 		}
 		else
 		{
-			//Step 1.3
+			
 			int connect_return = connect( internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
 			if( connect_return == -1 )
 			{
@@ -189,7 +197,7 @@ struct addrinfo internet_address_setup;
 //----------------------------------------------------------------------------------
 
 
-int connection( int internet_socket , FILE *filePointer)
+int connection( int internet_socket , FILE *filePointer , char *client_address_string , int size)
 {
 	struct sockaddr_storage client_internet_address;
 	socklen_t client_internet_address_length = sizeof client_internet_address;
@@ -201,28 +209,39 @@ int connection( int internet_socket , FILE *filePointer)
 		exit( 3 );
 	}
 	
-	
+	else
+	{
 	    //get IP address from client
         if (client_internet_address.ss_family == AF_INET) // IPv4 address
 		{
             struct sockaddr_in* s = (struct sockaddr_in*)&client_internet_address;
-            inet_ntop(AF_INET, &s->sin_addr, client_address_string, sizeof(client_address_string));
+            inet_ntop(AF_INET, &s->sin_addr, client_address_string, size);
+			fputs("--------------------------------\n" , filePointer);
+			fputs("IPv6 Adress : " , filePointer);
+			fputs("???" , filePointer);
+			fputs("\n" , filePointer);
 			fputs("IPv4 Adress : " , filePointer);
 			fputs( client_address_string , filePointer);
 			fputs("\n" , filePointer);
+			fputs("--------------------------------\n" , filePointer);
         } 
 		
 		else   // IPv6 address
 		{   
             struct sockaddr_in6* s = (struct sockaddr_in6*)&client_internet_address;
-            inet_ntop(AF_INET6, &s->sin6_addr, client_address_string, sizeof(client_address_string));
+            inet_ntop(AF_INET6, &s->sin6_addr, client_address_string, size);
+			fputs("--------------------------------\n" , filePointer);
 			fputs("IPv6 Adress : " , filePointer);
 			fputs( client_address_string , filePointer);
 			fputs("\n" , filePointer);
+			fputs("IPv4 Adress : " , filePointer);
+			fputs("???" , filePointer);
+			fputs("\n" , filePointer);
+			fputs("--------------------------------\n" , filePointer);
         }
-
+        //client_address_string[sizeof(client_address_string)] = '\0';
         printf("Client IP address: %s\n", client_address_string);
-		
+	}	
  return client_socket;
 
 }
@@ -231,52 +250,157 @@ int connection( int internet_socket , FILE *filePointer)
 //----------------------------------------------------------------------------------
 
 
-void execution( int internet_socket ,char client_address_string )
-{ 
+void execution( int internet_socket ,const char *client_address_string ,  FILE *filePointer)
+{   
+    
 	int number_of_bytes_received = 0;
 	char buffer[1000];
-    char HTTP[120];
 
-    sprintf(HTTP,"GET /json/%s HTTP/1.0\r\nHost: ip-api.com\r\nConnection: close\r\n\r\n", client_address_string);
-    
+   
 	number_of_bytes_received = recv( internet_socket, buffer, ( sizeof buffer ) - 1, 0 );
 	if( number_of_bytes_received == -1 )
 	{
 		perror( "recv" );
+		
 	}
 		
 	else
 	{
 		buffer[number_of_bytes_received] = '\0';
-		printf( "Received : %s\n", buffer );
 	}
+	printf("Received = %s",buffer);
+	
+	
+	//HTTP execution
+    execution_HTTP( filePointer , client_address_string);
 	
 	int number_of_bytes_send = 0;
-	number_of_bytes_send = send( internet_socket,"SERVER" , 6, 0 );
-	if( number_of_bytes_send == -1 )
+    int totalBytesSend = 0;
+    char totalBytesSendStr[20];
+	
+
+  while(1)
 	{
-		perror( "send" );
+	 number_of_bytes_send = send( internet_socket,"\nSPAM\n" , 7, 0 );
+	 
+	 if( number_of_bytes_send == -1 )
+	 {
+		printf("Total Bytes Send : %d\n",totalBytesSend);
+		sprintf(totalBytesSendStr, "%d", totalBytesSend);
+		
+		fputs("--------------------------------\n" , filePointer);
+		fputs("Total Bytes Send : ",filePointer );
+		fputs(totalBytesSendStr,filePointer);
+		fputs("\n" , filePointer );
+		fputs("--------------------------------\n" , filePointer);
+		break;
+	 }
+	 else
+	 {
+		 totalBytesSend += number_of_bytes_send;
+		 number_of_bytes_send = 0;
+		 usleep(100000);
+	 }
 	}
- 
+   
 }
 
 
 //----------------------------------------------------------------------------------
 
+void execution_HTTP( FILE *filePointer , const char *client_address_string)
+{
+	
+	int HTTP_socket = initialization_HTTP();
+	char buffer[1000];
+	char HTTP[200] ={0};
+	
+		
+    sprintf(HTTP,"GET /json/%s HTTP/1.0\r\nHost: ip-api.com\r\nConnection: close\r\n\r\n", client_address_string);
+   
 
-void cleanup( int internet_socket, int client_internet_socket ,int HTTP_socket )
+    printf("HTTP = %s\n",HTTP);
+	int number_of_bytes_send = 0;
+	number_of_bytes_send = send( HTTP_socket, HTTP , strlen(HTTP), 0 );
+    //number_of_bytes_send = send( HTTP_socket, "GET /json/fe80::3e10:67d5:f983:9996 HTTP/1.0\r\nHost: ip-api.com\r\nConnection: close\r\n\r\n" , 86, 0 );
+    if( number_of_bytes_send == -1 )
+    {
+        perror( "send" );
+    }
+	else
+	{
+		printf("\nGestuurd : %s\n",HTTP);
+	}
+	printf("na gestuurd\n");
+	
+    int number_of_bytes_received = 0;
+    number_of_bytes_received = recv( HTTP_socket, buffer, ( sizeof buffer ) - 1, 0 ); //Geen recv
+	printf("%d",number_of_bytes_received);
+    if( number_of_bytes_received == -1 )
+    {
+		
+        perror( "recv" );
+    }
+    else
+    {
+        buffer[number_of_bytes_received] = '\0';
+        printf( "Received HTTP : %s\n", buffer );
+    }
+	
+
+    char* jsonFile = strchr(buffer,'{');
+
+    if( jsonFile == NULL)
+	{
+        number_of_bytes_received = recv( HTTP_socket, buffer, ( sizeof buffer ) - 1, 0 );
+        if( number_of_bytes_received == -1 )
+        {
+            perror( "recv" );
+        }
+        else
+        {
+            buffer[number_of_bytes_received] = '\0';
+            printf( "Received2 : %s\n", buffer );
+        }
+        fputs("--------------------------------\n" , filePointer);
+        fputs("Geolocation = ",filePointer);
+        fputs( buffer , filePointer );
+        fputs("\n",filePointer);
+		fputs("--------------------------------\n" , filePointer);
+    } 
+	else
+	{
+		fputs("--------------------------------\n" , filePointer);
+        fputs("Geolocation = ",filePointer);
+        fputs(jsonFile, filePointer);
+        fputs("\n",filePointer);
+		fputs("--------------------------------\n" , filePointer);
+    }
+
+
+
+
+
+	int shutdown_return = shutdown( HTTP_socket, SD_RECEIVE );
+	if( shutdown_return == -1 )
+	{
+		perror( "shutdown" );
+	}
+	
+    close( HTTP_socket );
+}
+
+//----------------------------------------------------------------------------------
+
+void cleanup(int client_internet_socket)
 {
 	int shutdown_return = shutdown( client_internet_socket, SD_RECEIVE );
 	if( shutdown_return == -1 )
 	{
 		perror( "shutdown" );
 	}
-     shutdown_return = shutdown( HTTP_socket, SD_RECEIVE );
-	if( shutdown_return == -1 )
-	{
-		perror( "shutdown" );
-	}
-    close( HTTP_socket );
+
+   
 	close( client_internet_socket );
-	close( internet_socket );
+	
 }
